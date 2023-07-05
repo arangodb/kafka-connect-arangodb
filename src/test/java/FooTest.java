da4192e2c36e90af3508983069453454c781ba93
Michele Rastelli <michele@arangodb.com>
2023-07-05 14:36:36 +0200

    setup maven project

---
+++ src/test/java/FooTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FooTest {
+
+    @Test
+    void test() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+
+}
