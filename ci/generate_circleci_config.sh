b8f1834331529fb4ce3ad1b56a4977105bd32cb9
Vitaly Pryakhin <vitaly@arangodb.com>
2023-11-23 21:28:47 +0200

    only run tests using stable versions of ArangoDB from 3.11 and higher

--- ci/generate_circleci_config.sh
+++ ci/generate_circleci_config.sh
@@ -2,10 +2,10 @@
 set -o errexit -o pipefail
 
 # Getting a list of releases from the release tracker
-versions_json=$(curl -X 'GET' "https://$RELEASE_TRACKER_USERNAME:$RELEASE_TRACKER_PASSWORD@release-tracker.arangodb.com/list-releases-for-stable-branches?os=linux&arch=x86_64" -H 'accept: application/json')
+versions_json=$(curl -X 'GET' "https://$RELEASE_TRACKER_USERNAME:$RELEASE_TRACKER_PASSWORD@release-tracker.arangodb.com/list-releases-for-stable-branches?os=linux&arch=x86_64&minimum=3.11.0" -H 'accept: application/json')
 
 # Filter out the latest release for each stable branch
-latest_versions=$(echo "$versions_json" | jq -r '[keys[] as $k | "\(.[$k] | .[-1])"]')
+latest_versions=$(echo "$versions_json" | jq -r '[keys[] as $k | "\(.[$k] | .[-1])" | select(. != "null")]')
 
 echo "Installing yq..."
 sudo snap install yq
