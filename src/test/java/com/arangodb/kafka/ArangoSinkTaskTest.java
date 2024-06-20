10082d1595483506935fc92f4e24169ac29b7f58
Michele Rastelli <michele@arangodb.com>
2024-06-20 10:48:23 +0200

    [DE-814] CI restructure (#36)

--- src/test/java/com/arangodb/kafka/ArangoSinkTaskTest.java
+++ src/test/java/com/arangodb/kafka/ArangoSinkTaskTest.java
@@ -8,6 +8,6 @@ public class ArangoSinkTaskTest {
     @Test
     void version(){
         String version = new ArangoSinkTask().version();
-        assertThat(version).isEqualTo("1.2.0");
+        assertThat(version).isEqualTo("1.3.0-SNAPSHOT");
     }
 }
