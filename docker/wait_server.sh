6fce82eb220164138131e2f491289b2eb2da5d97
Wilfried Goesgens <willi@arangodb.com>
2025-10-21 13:25:49 +0200

    QA-712: add script to invoke from circle ci to wait for kafka to become ready (#48)
    * add script to invoke from circle ci
    
    * use full path

---
+++ docker/wait_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wait_server() {
+    # shellcheck disable=SC2091
+    until $(/usr/bin/nc -z $1 $2); do
+        printf '.'
+        sleep 1
+    done
+}
+a=$@
+while test -n "$a"; do
+    HOST=$1
+    PORT=$2
+    wait_server "${HOST}" "${PORT}"
+    shift 2
+    a=$@
+done
